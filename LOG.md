18deee62f7ae35eba784d0c26f87d22233843d3a
mattia richetto <mrichetto@opentable.com>
2017-02-23 12:22:49 -0800

    unit tests w/ jest

---
+++ LOG.md
@@ -0,0 +1,18 @@
+# Snapshot Test Everything
+
+- [X] Use yarn
+
+- [X] Add jest
+
+- [X] Add eslint-config-airbnb (and deps)
+
+- [X] Add babel (and deps)
+
+- [ ] ...
+
+## How to
+
+```bash
+yarn
+yarn jest
+```
---
+++ components/hello-world/server.spec.js
@@ -0,0 +1,18 @@
+import { data } from './server';
+
+const scenarios = [
+  { name: null },
+  { name: undefined },
+  { name: '' },
+  { name: 'Jane Doe' },
+  { name: 'Zlatan Ibrahimović' }
+];
+
+scenarios.forEach((scenario) => {
+  test(`when name is "${scenario.name}"`, () => {
+    const context = { params: { name: scenario.name } };
+    data(context, (error, model) => {
+      expect(model).toMatchSnapshot();
+    });
+  });
+});
