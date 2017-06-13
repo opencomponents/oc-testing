0e06d76b9e4a7bae52f86478a1244dc1f88cd33a
mattia richetto <mattia.richetto@gmail.com>
2017-06-12 19:44:26 -0700

    packages

--- components/hello-world/_package/template.js
+++ components/hello-world/_package/template.js
@@ -1,14 +1,1 @@
-var oc = oc || {};
-(oc.components = oc.components || {}), (oc.components[
-  '1c141eff044d83160448ef227d62cafec2a6e7eb'
-] = function(e) {
-  var n,
-    o = [],
-    c = e || {};
-  return function(e) {
-    o.push('<div>Hello ' + jade.escape(null == (n = e) ? '' : n) + '</div>');
-  }.call(
-    this,
-    'name' in c ? c.name : 'undefined' != typeof name ? name : void 0
-  ), o.join('');
-});
+var oc=oc||{};oc.components=oc.components||{},oc.components["1c141eff044d83160448ef227d62cafec2a6e7eb"]=function(e){var n,o=[],c=e||{};return function(e){o.push("<div>Hello "+jade.escape(null==(n=e)?"":n)+"</div>")}.call(this,"name"in c?c.name:"undefined"!=typeof name?name:void 0),o.join("")};
